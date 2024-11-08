838817ef697d094c284f400413824671f6ecfcd9
Hadi ZareZadeh <zarehadi2@gmail.com>
2024-11-08 18:35:38 +0330

    f

--- clone.bat
+++ clone.bat
@@ -1,1 +1,11 @@
-git clone --strip-components=1 https://github.com/hadiZareZadeh/plato-appium .
+@echo off
+set REPO_URL=https://github.com/hadiZareZadeh/plato-appium
+
+if exist temp (
+    rmdir /s /q temp
+)
+
+git clone %REPO_URL% temp
+move temp\* .
+move temp\.* .
+rmdir /s /q temp
